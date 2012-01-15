7136e3eb0359bb63d03cfd408651ec84861b52d3
Osamu Nakashima <o.nakashima@gmail.com>
2012-01-15 17:38:37 +0900

    Subjectの名前を勘違いして付けていたので修正。 Subjectの登録メソッドを実装

--- LifeGame/src/com/kayumidome/lifegame/Model/Engine.java
+++ LifeGame/src/com/kayumidome/lifegame/Model/Engine.java
@@ -1,9 +1,34 @@
 package com.kayumidome.lifegame.Model;
 import java.util.Observable;
+import java.util.Observer;
 
 public class Engine {
 	
-	private class StatusUpdateObserver extends Observable {
+	private final StatusUpdateSubject statusUpdateSubject = new StatusUpdateSubject();
+	private final CalculateExecuteSubject calculateExecuteSubject = new CalculateExecuteSubject();
+	private final GridUpdateSubject gridUpdateSubject = new GridUpdateSubject();
+	private final AbortSubject abortSubject = new AbortSubject();
+	
+	public Engine() {
+	}
+	
+	public void setStatusUpdateObserver(Observer observer) {
+		this.statusUpdateSubject.addObserver(observer);
+	}
+	
+	public void setCalculateExecuteObserver(Observer observer) {
+		this.calculateExecuteSubject.addObserver(observer);
+	}
+	
+	public void setGridUpdateObserver(Observer observer) {
+		this.gridUpdateSubject.addObserver(observer);
+	}
+	
+	public void setAbortObserver(Observer observer) {
+		this.abortSubject.addObserver(observer);
+	}
+	
+	private class StatusUpdateSubject extends Observable {
 		private EngineStatus.Status mStat;
 		
 		@Override
@@ -23,7 +48,7 @@ public class Engine {
 		}
 	}
 	
-	private class CalculateExecuteObserver extends Observable {
+	private class CalculateExecuteSubject extends Observable {
 		@Override
 		public void  notifyObservers(Object arg) {
 			this.setChanged();
@@ -32,7 +57,7 @@ public class Engine {
 		}
 	}
 	
-	private class GridUpdateObserver extends Observable {
+	private class GridUpdateSubject extends Observable {
 		@Override
 		public void  notifyObservers(Object arg) {
 			this.setChanged();
@@ -41,7 +66,7 @@ public class Engine {
 		}
 	}
 	
-	private class AbortObserver extends Observable {
+	private class AbortSubject extends Observable {
 		@Override
 		public void  notifyObservers(Object arg) {
 			this.setChanged();
