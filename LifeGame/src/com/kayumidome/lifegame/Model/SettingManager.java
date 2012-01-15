1bf053dbedbdf27cd99f2ffc86702e570da418f7
Osamu Nakashima <o.nakashima@gmail.com>
2012-01-15 18:32:29 +0900

    設定マネージャのインタフェースメソッドを空で作成

--- LifeGame/src/com/kayumidome/lifegame/Model/SettingManager.java
+++ LifeGame/src/com/kayumidome/lifegame/Model/SettingManager.java
@@ -1,5 +1,30 @@
 package com.kayumidome.lifegame.Model;
 
 public class SettingManager {
+	
+	public enum Speed {
+		slow,
+		normal,
+		high,
+		max,
+	}
 
+	public SettingManager() {
+		//todo:設定ファイルから情報を読み込んだり、ファイルがなければ作ったり
+	}
+	
+	public void Save() {
+		//todo:現在の設定をファイルに保存
+	}
+	
+	private Speed speed;
+	
+	public Speed getSpeed() {
+		return this.speed;
+	}
+	
+	public void setSpeed(Speed speed) {
+		this.speed = speed;
+		//todo:スピード変更に伴う処理
+	}
 }
